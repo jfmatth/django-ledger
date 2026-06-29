67adf52352a374eb3ef03322f0aad7b8f40546f7
John Matthew <john@compunique.com>
2026-06-28 22:35:07 -0500

    more tests, code and files

--- ledger/tests.py
+++ ledger/tests.py
@@ -4,63 +4,92 @@ import os
 
 from django.test import TestCase
 
-from ledger.command_loader import buildTransactions
-from ledger.models import RawCSV, RawTransaction
+import ledger.command_loader as loader
 
-"""
-Testing all the callings of loader management command
+from ledger.models import RawCSV, RawTransaction, Ledger
 
-[X]loader.loadCSV(options['filename'][0])
-[ ]loader.buldTransactions()
-[ ]loader.buildStrikeInfo()
-[ ]loader.updateLedger()
+import logging
+logger = logging.getLogger(__name__)
 
-"""
+FILE_SIMPLECSP = 'tests/simple-csp.csv'
 
-rows = [
-    ['Date', 'Action', 'Symbol', 'Description', 'Quantity', 'Price', 'Fees & Comm', 'Amount'],
-    ['07/25/2025', 'Sell to Open', 'T 08/22/2025 29.00 C', 'CALL AT&T INC $29 EXP 08/22/25', '15', '$0.34', '$9.95', '$500.05'],
-    ['07/25/2025', 'Buy to Open', 'LCID 09/19/2025 2.50 P', 'PUT LUCID GROUP INC $2.5 EXP 09/19/25', '10', '$0.21', '$6.61', '-$216.61']
-]
-
-flatten_rows = lambda lst: '\n'.join([','.join(row) for row in lst])
-TRANSACTION_COUNT = 2
-
-def loadRows():
-    # Creates a RawCSV record to read from
-    record = RawCSV()
-    record.filename = "test"
-    record.data = flatten_rows(rows)
-    record.ingested = False
-    record.save()
-
-
-class loadCSVTestCase(TestCase):
-    """
-        Since we don't read in a file, we fake it here with loadRows()
-    """
+class loaderTest(TestCase):
     def setUp(self):
-        loadRows()
+        loader.loadCSV(FILE_SIMPLECSP)
 
-    def test_1_csv_load(self):
-        r = RawCSV.objects.all()[0]
+    def test_1_csvrecord(self):
+        # once loaded, we should have one record in the RawCSV table
+        self.assertEqual(RawCSV.objects.all().count(), 1)
 
-        self.assertEqual(r.filename, "test")
-        self.assertEqual(r.ingested, False)
-        self.assertEqual(len(RawCSV.objects.all()), 1)
+    def test_1a_fields(self):
+        # check all the fields are loaded too
+        self.assertEqual(RawCSV.objects.get(pk=1).filename, FILE_SIMPLECSP) 
+        self.assertEqual(RawCSV.objects.get(pk=1).ingested, False)
 
-    def tearDown(self):
-        pass
+    def test_2_buildTransactions(self):
+        loader.buildTransactions()
 
+        # All transactions are built, so see if there are two transactions
+        self.assertEqual(RawTransaction.objects.all().count(),2)
 
-class buildTransactionTestCase(TestCase):
-
+# These are full body tests, for each option case scenario
+class simpleCSP(TestCase):
     def setUp(self):
-        loadRows()
-        buildTransactions()
+        loader.loadCSV(FILE_SIMPLECSP)
+        loader.buildTransactions()
+        loader.buildStrikeInfo()
+        loader.updateLedger()
         
-    def test_1_buildTransaction(self):
-        ## We load the CSV and Build the RawTransaction objects
-        self.assertEqual(len(RawTransaction.objects.all() ), TRANSACTION_COUNT)
+    def test_1(self):
+        # Verify that a simple
+        rec = Ledger.objects.get(pk=1)
 
-    def test_2
+        self.assertEqual(Ledger.objects.all().count(), 1)
+        self.assertEqual(rec.status, "Closed")
+        self.assertEqual(rec.quantity, 0)
+
+# rows = [
+#     ['Date', 'Action', 'Symbol', 'Description', 'Quantity', 'Price', 'Fees & Comm', 'Amount'],
+#     ['07/25/2025', 'Sell to Open', 'T 08/22/2025 29.00 C', 'CALL AT&T INC $29 EXP 08/22/25', '15', '$0.34', '$9.95', '$500.05'],
+#     ['07/25/2025', 'Buy to Open', 'LCID 09/19/2025 2.50 P', 'PUT LUCID GROUP INC $2.5 EXP 09/19/25', '10', '$0.21', '$6.61', '-$216.61']
+# ]
+
+# flatten_rows = lambda lst: '\n'.join([','.join(row) for row in lst])
+# TRANSACTION_COUNT = 2
+
+# def loadRows():
+#     # Creates a RawCSV record to read from
+#     record = RawCSV()
+#     record.filename = "test"
+#     record.data = flatten_rows(rows)
+#     record.ingested = False
+#     record.save()
+
+
+# class loadCSVTestCase(TestCase):
+#     """
+#         Since we don't read in a file, we fake it here with loadRows()
+#     """
+#     def setUp(self):
+#         loadRows()
+
+#     def test_1_csv_load(self):
+#         r = RawCSV.objects.all()[0]
+
+#         self.assertEqual(r.filename, "test")
+#         self.assertEqual(r.ingested, False)
+#         self.assertEqual(len(RawCSV.objects.all()), 1)
+
+#     def tearDown(self):
+#         pass
+
+
+# class buildTransactionTestCase(TestCase):
+
+#     def setUp(self):
+#         loadRows()
+#         buildTransactions()
+        
+#     def test_1_buildTransaction(self):
+#         ## We load the CSV and Build the RawTransaction objects
+#         self.assertEqual(len(RawTransaction.objects.all() ), TRANSACTION_COUNT)
