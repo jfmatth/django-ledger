8a52cb36bed34b6c8951a3b5ea2820818cf25ac6
John Matthew <john@compunique.com>
2026-06-29 15:58:59 -0500

    fix logger settings

--- core/settings.py
+++ core/settings.py
@@ -143,25 +143,25 @@ LOGGING = {
             'formatter': 'verbose',  # Use the 'verbose' formatter
         },
     },
-    # 'root': {
-    #     'handlers': ['console'],
-    #     'level': 'DEBUG',  # Root logger level
-    # },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',  # Root logger level
+    },
     'loggers': {
         'django': {
             'handlers': ['console'],
             'level': 'INFO',  # Django-specific logger level
             'propagate': False,
         },
-        # 'core': {
-        #     'handlers': ['console'],
-        #     'level': 'DEBUG',  # Django-specific logger level
-        #     'propagate': True, 
-        # },
+        'core': {
+            'handlers': ['console'],
+            'level': 'DEBUG',  # Django-specific logger level
+            'propagate': False, 
+        },
         'ledger': {
             'handlers': ['console'],
             'level': 'INFO',  # Django-specific logger level
-            'propagate': True, 
+            'propagate': False, 
         },
 
     },
